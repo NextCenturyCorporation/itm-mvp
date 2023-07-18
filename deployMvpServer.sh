d36bb583c185ea3bb044ad9bbbba72db2a7d9230
Will Gossard <william.gossard@caci.com>
2023-07-18 09:05:29 -0400

    adding ability to deploy 1 or both

--- deployMvpServer.sh
+++ deployMvpServer.sh
@@ -4,11 +4,54 @@ MVP_SERVER=10.216.38.88
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-ssh $MVP_SERVER docker kill itm-server; 
-ssh $MVP_SERVER docker rm itm-server
-ssh $MVP_SERVER docker run -d -p 8080:8080 --name itm-server itm-server
+deployServer() {
+    ssh $MVP_SERVER docker kill itm-server; 
+    ssh $MVP_SERVER docker rm itm-server;
+    ssh $MVP_SERVER docker run -d -p 8080:8080 --name itm-server itm-server;
+}
 
+deployDashbaord() {
+    ssh $MVP_SERVER docker-compose -f /home/ec2-user/github/itm-mvp/itm_dashboard/docker_setup/docker-compose.yml down
+    scp ./itm_dashboard/dashboard-ui/public/configs/prod/config.js $MVP_SERVER:/home/ec2-user/github/itm-mvp/itm_dashboard/dashboard-ui/public/configs/prod/config.js
+    ssh $MVP_SERVER docker-compose -f /home/ec2-user/github/itm-mvp/itm_dashboard/docker_setup/docker-compose.yml up -d
+}
 
-ssh $MVP_SERVER docker-compose -f /home/ec2-user/github/itm-mvp/itm_dashboard/docker_setup/docker-compose.yml down
-scp ./itm_dashboard/dashboard-ui/public/configs/prod/config.js $MVP_SERVER:/home/ec2-user/github/itm-mvp/itm_dashboard/dashboard-ui/public/configs/prod/config.js
-ssh $MVP_SERVER docker-compose -f /home/ec2-user/github/itm-mvp/itm_dashboard/docker_setup/docker-compose.yml up -d
+usage()
+{
+   # Display Help
+   echo "Deploys resources to EC2."
+   echo
+   echo "Syntax: scriptTemplate [-s|d]"
+   echo "options:"
+   echo "s     Deploys itm-server"
+   echo "d     Deploys itm-dashboard"
+   echo
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -s | --server )   
+            shift
+            deployServer
+            ;;
+        -d | --dashboard )   
+            shift
+            deployDashbaord
+            ;;
+        -sd | -ds )
+            shift
+            deployServer 
+            deployDashbaord
+            ;;
+        -h | --help )           
+            usage
+            exit
+            ;;
+        * )                     
+            usage
+            exit 1
+    esac
+    shift
+done
+
+        
